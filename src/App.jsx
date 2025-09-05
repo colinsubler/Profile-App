fe3bba2a9fdfbe5427682b32c9c2c6b368b9a6ac
colinsubler <csubler@purdue.edu>
2025-09-05 10:04:43 -0400

    Added wrapper class

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,46 @@ import imgOne from './assets/woman1.png';
 import imgTwo from './assets/man1.png';
 import imgThree from './assets/man2.png';
 import About from './components/About';
+import Wrapper from './components/Wrapper';
+
+const profiles = [
+    {
+        imgSrc: imgOne,
+        title: "Janet Smith",
+        description: "CEO"
+    },
+    {
+        imgSrc: imgTwo,
+        title: "Thomas Johnson",
+        description: "CFO"
+    },
+    {
+        imgSrc: imgThree,
+        title: "Roger Williams",
+        description: "COO"
+    }
+];
 
 function App() {
     return (
         <>
             <Header />
-            <div className="card-row">
-              <About />
-            </div>
-            <div className="card-row">
-              <Card
-                  imgSrc={imgOne}
-                  title="Janet Smith"
-                  description="CEO"
-              />
-              <Card
-                  imgSrc={imgTwo}
-                  title="Thomas Johnson"
-                  description="CFO"
-              />
-              <Card
-                  imgSrc={imgThree}
-                  title="Roger Williams"
-                  description="COO"
-              />
-            </div>
-            <div className='placeholder'></div>
+            <Wrapper>
+                <div className="card-row">
+                    <About />
+                </div>
+                <div className="card-row" id="profiles">
+                    {profiles.map((profile, index) => (
+                        <Card
+                            key={index}
+                            imgSrc={profile.imgSrc}
+                            title={profile.title}
+                            description={profile.description}
+                        />
+                    ))}
+                </div>
+                <div className='placeholder'></div>
+            </Wrapper>
         </>
     );
 }
